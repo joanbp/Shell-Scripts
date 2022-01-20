11b126c0acadc0fe975ef0f243ad6ef9a4e46a8b
Joanes B Prasatya <joanbp@yahoo.com>
2022-01-20 09:56:50 +0800

    Update gzip_asmaudit.log.1.sh

--- gzip_asmaudit.log.1.sh
+++ gzip_asmaudit.log.1.sh
@@ -3,6 +3,12 @@
 # Desc : Find and gzip /var/log/asmaudit.log.1 if it's > 1.0 GB
 
 threshold=1073741824
+
+if [ ! -f "/var/log/asmaudit.log.1" ]; then
+    echo "asmaudit.log.1 does not exist."
+    exit 0
+fi
+
 FileSizeCheck=$(stat -c %s "/var/log/asmaudit.log.1")
 
 if [ $FileSizeCheck -gt $threshold ]
