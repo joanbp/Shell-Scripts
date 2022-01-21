4e655a88d2ec32195c4c9fbb975994fdc3dc65da
Joanes B Prasatya <joanbp@yahoo.com>
2022-01-21 12:49:01 +0800

    Update gzip_asmaudit.log.1.sh

--- gzip_asmaudit.log.1.sh
+++ gzip_asmaudit.log.1.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 # File : gzip_asmaudit.log.1.sh
 # Desc : Find and gzip /var/log/asmaudit.log.1 if it's > 1.0 GB
+#        Find and nullify /var/log/asmaudit.log if it's > 1.5 GB
 
-threshold=1073741824
+threshold1=1073741824
+threshold2=1610612736
 
-if [ ! -f "/var/log/asmaudit.log.1" ]; then
-    echo "asmaudit.log.1 does not exist."
-    exit 0
+# Find and gzip /var/log/asmaudit.log.1 if it's > 1.0 GB
+if [ -f "/var/log/asmaudit.log.1" ]; then
+	FileSizeCheck=$(stat -c %s "/var/log/asmaudit.log.1")
+
+	if [ $FileSizeCheck -gt $threshold1 ]
+	then
+   		find /var/log -iname "asmaudit.log.1" -exec gzip {} \;
+	fi
 fi
 
-FileSizeCheck=$(stat -c %s "/var/log/asmaudit.log.1")
-
-if [ $FileSizeCheck -gt $threshold ]
-then
-   find /var/log -iname "asmaudit.log.1" -exec gzip {} \;
+# Find and nullify /var/log/asmaudit.log if it's > 1.5 GB
+if [ -f "/var/log/asmaudit.log" ]; then
+	FileSizeCheck=$(stat -c %s "/var/log/asmaudit.log")
+	
+        if [ $FileSizeCheck -gt $threshold2 ]
+	then
+   		cat /dev/null > /var/log/asmaudit.log
+	fi
 fi
